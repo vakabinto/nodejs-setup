8dd4e4744204f87e8918239676816968deb715ba
vakabinto <vakabinto@gmail.com>
2024-04-08 04:32:07 +0100

    iac

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "node-setup00012"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "test"
+  }
+}
+#
