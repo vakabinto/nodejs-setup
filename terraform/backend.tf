87ad7ba8d10023e1799539120f271d31c6aa3940
vakabinto <vakabinto@gmail.com>
2024-04-08 06:04:09 +0100

    vf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "test"
+    dynamodb_table = "testNG"
   }
 }
 #
